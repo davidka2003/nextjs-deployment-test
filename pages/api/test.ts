8f1071d0c1f4d5f030e200d7cd57dc18dd19da62
davidka2003 <d4v1ds0n.p@gmail.com>
2023-11-27 09:21:23 +0100

    +some deps

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,8 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ITest } from "@/types";
+import { serializeError } from "@/utils";
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ITest>) {
   const { data } = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+  serializeError();
   res.status(200).json(data);
 }
